4a3d2273bba467794956055ec9e13ba74888974b
Icegotcha Fantasoxy <icekotcha@gmail.com>
2018-12-31 15:38:31 +0700

    Add Fireworks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Timer from './Components/Timer.jsx';
+import Fireworks from './Components/Fireworks.jsx';
 import './Styles/App.scss';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
           <div className="stars-layer1"/>
           <div className="stars-layer2"/>
           <div className="stars-layer3"/>
+          <Fireworks/>
           <Timer/>
           <div className="until2019">Until 2019</div>
         </header>
---
+++ src/Components/Fireworks.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import '../Styles/Fireworks.scss'
+
+class Fireworks extends Component {
+  render() {
+    return (
+      <div className='start'>
+        <div className="before"/>
+        <div className="after"/>
+      </div>
+    );
+  }
+}
+
+export default Fireworks;
